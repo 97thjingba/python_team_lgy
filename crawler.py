736b979574091ad82842ab79e60c8c487c1d8b91
952079739 <42928709+952079739@users.noreply.github.com>
2018-10-23 18:48:17 +0800

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -1,12 +1,16 @@
 from urllib.request import urlopen
 import re
 html = urlopen("http://thepokerlogic.com/glossary").read().decode('utf-8')
-print(html)
+
 reh = re.findall(r"<h1>(.*?)</h1>", html)
-print("h1\n", reh)
+
 ret = re.findall(r"<head>(.*?)</head>", html)
-print("title\n", ret)
+
 res = re.findall(r"<p>(.*?)</p>", html)
-print("p\n", res)
+
 recs = re.findall(r'href="(.*?)"', html)
-print("href\n", recs)
+rec = re.findall(r'<li>="(.*?)'.html)
+print(rec)
+
+
+# print(html, "\n", reh, "\n", ret, "\n", res, "\n", recs)
