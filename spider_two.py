663f45465a514eb1d6aca78bcc5f630dd7931482
952079739 <952079739@qq.com>
2018-11-07 18:25:14 +0800

    更改后

--- spider_two.py
+++ spider_two.py
@@ -5,10 +5,11 @@ import json
 def parse_html(text):
     soup = BeautifulSoup(text, 'lxml')
     url_href = soup.select('.content_a a')
-    allurl = []
+    all_url = []
     for url in url_href:
-        allurl.append(url['href'])
-    for href in allurl:
+        all_url.append(url['href'])
+    all_url2 = sorted(set(all_url), key=all_url.index)
+    for href in all_url2:
         url_new = 'http://thepokerlogic.com' + str(href)   #拼接链接
         data = requests.get(url_new).text
         soup1 = BeautifulSoup(data, 'lxml')
@@ -22,38 +23,40 @@ def parse_html(text):
             h2_title = h2_text[0].string
         content_a = soup1.select('.relevant-content a')
         for c in content_a:
-            content_text = c.string
+            content_text = [c.string, c['href']]
         study = soup1.select('.study-content a')
         if len(study) == 0:
             study_text = '无'
         else:
             for i in study:
-                study_text = i.string
+                study_text = [i.string, i['href']]
+
 
         h3_title = h3_text[0].string
         explain = p_text[4].string
         example = h4_text[0].string
         p_text = p_text[5].string
         h4_text = h4_text[1].string
+
         """
         建立字典
         """
 
         result = {
-            'first_title': h2_title,
-            'second_title': h3_title,
-            'title_explain': explain,
-            'Illustrate': example,
+            'title': h2_title,
+            'describe_title': h3_title,
+            'describe_content': explain,
+            'illustrate_title': example,
             'Illustrate_content': p_text,
-            'Concept': h4_text,
-            'Concept_content': content_text,
+            'relevant_title': h4_text,
+            'relevant_content': content_text,
             'Study_content': study_text,
-        }
+         }
         write_file(result)
 
 
 def write_file(data):
-    with open('spider_text.json', 'a', encoding='utf-8') as f:
+    with open('spider.json', 'a', encoding='utf-8') as f:
         f.write('\n' + json.dumps(data, ensure_ascii=False))
         f.close()
 
